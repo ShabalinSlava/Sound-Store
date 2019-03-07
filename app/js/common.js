12634a3cdd872d622674de41e3ef1462ad9b27e3
Shabalin Slava <svaznoy0309@gmail.com>
2019-03-07 22:52:55 +0800

    connecting mechanics to navigation

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,37 @@
 $(function() {
+  $("#search").click(function() {
+    $("#search-on-container").addClass("visible");
+  });
 
-	// Custom JS
+  $("#search-close").click(function() {
+    $("#search-on-container").removeClass("visible");
+  });
 
+  $(document).click(function() {
+    $("#search-line").focus();
+  });
+
+  $("#shopping-cart").click(function(event) {
+    $("#information").addClass("visible");
+  });
+
+  $("#information-close").click(function(event) {
+    $("#information").removeClass("visible");
+  });
+
+  $("#country").niceSelect();
+
+  $("#delivers-mode").niceSelect();
+
+  $(".consent-block").click(function() {
+    $(".consent-block").toggleClass("box-agree");
+  });
+
+  $("#favorites").click(function(event) {
+    $("#favorites-container").addClass("visible");
+  });
+
+  $("#favorites-close").click(function(event) {
+    $("#favorites-container").removeClass("visible");
+  });
 });
